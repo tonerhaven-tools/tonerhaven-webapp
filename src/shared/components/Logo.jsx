1cf93764af30517d8f08ede210e5f4dc839bf7a4
jesse-frc <jesse@flatratecopiers.com>
2023-09-03 03:50:08 -0400

    Just the website logo

---
+++ src/shared/components/Logo.jsx
@@ -0,0 +1,9 @@
+export default function SiteLogo() {
+  return (
+      <span>
+        <img src={"/images/favicon.png"} style={{width:"40px"}}/>
+        <b><span className={"text-blue"}>Toner</span></b>
+        <b><span className={"text-red"}>Haven</span></b>
+      </span>
+  )
+}
