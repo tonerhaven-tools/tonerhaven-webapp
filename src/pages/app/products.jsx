4d74108b84956e3cef950894479f4a7ab1f07c6f
jesse-frc <jesse@flatratecopiers.com>
2023-09-03 03:53:33 -0400

    Added products.jsx

---
+++ src/pages/app/products.jsx
@@ -0,0 +1,17 @@
+import { Layout, Page } from "@/shared/components";
+import { Container } from "react-bootstrap";
+
+export default function Products() {
+  return (
+    <Page title={"Toner Haven | All Products!"}>
+      <Layout>
+        <Container>
+
+          <h1>Products Page</h1>
+
+        </Container>
+      </Layout>
+    </Page>
+  );
+};
+
