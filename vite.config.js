e4ca8807656818e228d260dd0fded2f84178540f
Jesse Canonigo <strifejeyz@gmail.com>
2023-09-05 01:42:56 -0400

    Proxy reads hostname:port from .env the production should have its own .env file that has the correct hostname:port for the server

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), basicSsl()],
-  resolve: {
-    alias: {
-      "@": path.join(__dirname, "./src/"),
-      components: path.join(__dirname, "./src/shared/components/"),
-      pages: path.join(__dirname, "./src/pages/"),
-      hooks: path.join(__dirname, "./src/shared/hooks/"),
-      utils: path.join(__dirname, "./src/shared/utils/"),
+export default ({ mode }) => {
+  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+
+  return defineConfig({
+    plugins: [react(), basicSsl()],
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "./src/"),
+        components: path.join(__dirname, "./src/shared/components/"),
+        pages: path.join(__dirname, "./src/pages/"),
+        hooks: path.join(__dirname, "./src/shared/hooks/"),
+        utils: path.join(__dirname, "./src/shared/utils/"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, 'api')
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.EXPRESS_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, 'api')
+        }
       }
     }
-  }
-});
+  });
+}
