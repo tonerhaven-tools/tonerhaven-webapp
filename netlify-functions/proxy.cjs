de0273185e58eb72f13a9a566aedbfb9f079435d
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-05 21:20:19 +0800

    Update proxy.cjs

--- netlify-functions/proxy.cjs
+++ netlify-functions/proxy.cjs
@@ -1,26 +1,19 @@
 exports.handler = async (event, context) => {
   try {
     // Define the URL of the external API
-    const apiUrl = "https://tonerhaven-api.netlify.app";
+    const apiUrl = "https://tonerhaven.netlify.app";
 
-    // Extract query parameters from the incoming request
-
-    // Dynamically import node-fetch as an ES module
     const fetch = await import("node-fetch");
 
-    // Determine the request method (GET, POST, PUT, etc.)
     const httpMethod = event.httpMethod.toUpperCase();
 
-    // Initialize options object with common headers
     const options = {
       method: httpMethod,
       headers: {
         "Content-Type": "application/json",
-        // Add any additional headers if needed
       },
     };
 
-    // Include request body for POST and PUT requests
     if (httpMethod === "POST" || httpMethod === "PUT") {
       options.body = event.body;
     }
@@ -28,7 +21,6 @@ exports.handler = async (event, context) => {
     const url = `${apiUrl}${event.path}`;
     console.log(url);
 
-    // Make a request to the external API
     const response = await fetch.default(url, options);
 
     if (!response.ok) {
