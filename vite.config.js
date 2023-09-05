6890a5e97d377d3c743c9319b4288e57967ff86c
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-05 22:03:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import basicSsl from "@vitejs/plugin-basic-ssl";
+require("dotenv").config();
 
 export default ({ mode }) => {
   process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
