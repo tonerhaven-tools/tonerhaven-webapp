9f2d9dc2907c6dc17b9f149989ca8332a3232781
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-07 12:33:48 +0800

    modified profile

--- src/shared/components/index.jsx
+++ src/shared/components/index.jsx
@@ -3,3 +3,4 @@ export { default as Layout } from "./Layout";
 export { default as Appbar } from "./Appbar";
 export { default as Footer } from "./Footer";
 export { default as Logo } from "./Logo";
+export { default as SecuredLayout } from "./auth/SecuredLayout";
