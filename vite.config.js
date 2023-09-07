c81a5bb8387e96b93ac1aafe0c80d533a3d721cb
Jesse Canonigo <strifejeyz@gmail.com>
2023-09-07 02:40:59 -0400

    Removed reverse-proxy.

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-require("dotenv").config();
 
-export default ({ mode }) => {
-  process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ""));
-
-  return defineConfig({
-    plugins: [react(), basicSsl()],
-    resolve: {
-      alias: {
-        "@": path.join(__dirname, "./src/"),
-        components: path.join(__dirname, "./src/shared/components/"),
-        pages: path.join(__dirname, "./src/pages/"),
-        hooks: path.join(__dirname, "./src/shared/hooks/"),
-        utils: path.join(__dirname, "./src/shared/utils/"),
-      },
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "./src/"),
+      components: path.join(__dirname, "./src/shared/components/"),
+      pages: path.join(__dirname, "./src/pages/"),
+      hooks: path.join(__dirname, "./src/shared/hooks/"),
+      utils: path.join(__dirname, "./src/shared/utils/"),
     },
-    server: {
-      proxy: {
-        "/api": {
-          target: process.env.EXPRESS_API_URL,
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, "/api"),
-        },
-      },
-    },
-  });
-};
+  },
+});
