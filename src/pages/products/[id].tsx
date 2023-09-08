99f29f2c3217de2555099066b9bbfd7feddd245a
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-09-09 00:27:37 +0800

    content spacer reduction

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,10 +1,18 @@
+import Layout from "@/shared/components/Layout";
+import Page from "@/shared/components/Page";
 import { useParams } from "react-router-dom";
 
-interface ProductDetailProps {}
+interface ProductDetailProps { }
 
 const ProductDetail: React.FC<ProductDetailProps> = () => {
   const { id } = useParams();
-  return <div>Test {id}</div>;
+  return (
+    <Page title="Toner Haven | Product Information">
+      <Layout header="Product Information">
+        <div>{id}</div>
+      </Layout>
+    </Page>
+  );
 };
 
 export default ProductDetail;
