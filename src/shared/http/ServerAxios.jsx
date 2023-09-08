d5b945cc595778f7ea6d532e7e48488df0ab2751
Jesse Canonigo <strifejeyz@gmail.com>
2023-09-08 01:14:32 -0400

    Added global headers for subseq requests.

--- src/shared/http/ServerAxios.jsx
+++ src/shared/http/ServerAxios.jsx
@@ -1,8 +1,13 @@
 import Axios from "axios";
+
 const server_url = import.meta.env.VITE_API_URL;
 const ServerAxios = Axios.create({
-  baseURL: server_url
+  baseURL: server_url,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  }
 });
 
 export default ServerAxios;
-export { server_url };
+export {server_url};
